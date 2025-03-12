f52b923dd508ed581306618ae54752f60dfdae83
marcus.ma <marcus.ma@optnet.site>
2025-03-12 19:39:15 +0800

    更新

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8c54da354fd1c13a0661cacf93f64a09",
+const RESOURCES = {"flutter_bootstrap.js": "7e503f8d4020fc5ba01a3a7d6f61f1b8",
 "version.json": "5e82a1b5316677f15a7f3e5a31265f34",
-"index.html": "7cd39888fc29d2e125d9569689af820b",
-"/": "7cd39888fc29d2e125d9569689af820b",
+"index.html": "add13853c54a133493b0f3e605caf553",
+"/": "add13853c54a133493b0f3e605caf553",
 "main.dart.js": "756b726d58177c92dfbca92b7b8025d6",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
